18eaa6160fd7e37da3d5f374f72ccfc54068124d
Aryan Mukerji <aryanmukerji@gmail.com>
2021-02-15 01:48:27 +0530

    First Commit (BRM_LAB_4)

---
+++ src/main/java/brm/Lab4/ClassJenkins.java
@@ -0,0 +1,10 @@
+package brm.Lab4;
+
+public class ClassJenkins {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" Hello Jenkins !! ");
+		System.out.println(" My Name Is Aryan Mukerji !! ");
+	}
+}
